608b3ad8ee33dbcb500c58bbc66f6bf7ee052d73
Indigo-Morgenstern <mzcarrots@gmail.com>
2026-01-24 15:09:32 +0100

    edited executor and wrote code for errors.c

--- errors.c
+++ errors.c
@@ -1,1 +1,23 @@
+void error_undefined_variable(void) {
+    // after trying to access a variable that hasn't yet been created with MAL
+    fprintf(stderr, "Tried to use a variable that does not exist.\n");
+    exit(0);
+}
 
+void error_out_of_memory(void) {
+    // after trying to allocate memory currently not available in the system
+    fprintf(stderr, "Not enough memory.\n");
+    exit(0);
+}
+
+void error_diff_length(void) {
+    // after trying to apply AND/XOR to variables of different length
+    fprintf(stderr, "Logic operation between sequences of different lengths.\n");
+    exit(0);
+}
+
+void error_wrong_memory(void) {
+    // after trying to access a cell which is not within the variable range
+    fprintf(stderr, "Wrong memory access.\n");
+    exit(0);
+}
--- executor.c
+++ executor.c
@@ -1,1 +1,74 @@
+void execute(Command cmd) {
 
+	switch(cmd.op) {
+//VERY WIP!!! switch/command notation is very wrong but actual function workings should be correct assuming the functions that are called also work. -gui
+		case ASS:
+			//assigns n to the first space of x
+			memory_write(var_get_start(cmd.a),cmd.b);
+			break;
+		case INC:
+			//writes in the nth space denoted by x,the same number add 1
+			memory_write(var_get_start(cmd.a)+cmd.b-1,memory_read(var_get_start(cmd.a)+cmd.b-1)+1);
+            break;
+		case DEC:
+			//writes in the nth space denoted by x, the same number minus 1
+			memory_write(var_get_start(cmd.a)+cmd.b-1,memory_read(var_get_start(cmd.a)+cmd.b-1)-1);
+            break;
+		case MAL:
+			//	use memory_allocate!! wait for implementation
+
+            break;
+
+		case PRI:
+			//reads the nth space denoted by x and prints it
+			printf("%d",memory_read(var_get_start(cmd.a)+cmd.b-1));
+            break;
+
+		case ADD:
+			//overwrites the first cell of x with the result of the addition of the first cells of x and y
+			memory_write(var_get_start(cmd.a),memory_read(var_get_start(cmd.a))+memory_read(var_get_start(cmd.b)));
+            break;
+
+		case SUB:
+			//overwrites the first cell of x with the result of the subtraction of the first cells of x and y
+			memory_write(var_get_start(cmd.a),memory_read(var_get_start(cmd.a))-memory_read(var_get_start(cmd.b)));
+            break;
+
+        case MUL:
+			//overwrites the first cell of x with the result of the multiplication of the first cells of x and y
+			memory_write(var_get_start(cmd.a),memory_read(var_get_start(cmd.a))*memory_read(var_get_start(cmd.b)));
+            break;
+
+		case AND:
+			//overwriting each element of x[i] with the value of (x[i]*y[i]%2) 
+			int i;
+			for(i=1,i=var_get_size(cmd.a),i++){
+				memory_write(var_get_start(cmd.a)+i-1,(memory_read(var_get_start(x)+i-1)*memory_read(var_get_start(cmd.b)+i-1))%2);
+			}
+            break;
+
+		case XOR:
+			//overwriting each element of x[i] with the value of (x[i]+y[i]%2)
+                        int i;
+                        for(i=1,i=var_get_size(cmd.a),i++){
+                                memory_write(var_get_start(cmd.a)+i-1,(memory_read(var_get_start(cmd.a)+i-1)+memory_read(var_get_start(cmd.b)+i-1))%2);
+                        }
+            break;
+        
+		case FRE:
+			//	use memory_free()!! wait for implementation
+            
+            break;
+
+		case PRA:
+			//prints contents of a variable
+			int i;
+			printf("[")
+			for(i=1,i=var_get_size(cmd.a),i++){
+				printf("%d ",memory_read(var_get_start(cmd.a)+i-1));
+			}
+			printf("]");
+            break;
+
+	}
+}
