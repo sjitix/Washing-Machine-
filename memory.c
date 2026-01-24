eacdbc8dae7bdd814d8be15c0ffe7d3971c5703c
sjitix <126064940+sjitix@users.noreply.github.com>
2026-01-24 17:03:10 +0100

    Refactor memory_init and add var_allocate function

--- memory.c
+++ memory.c
@@ -3,16 +3,34 @@
 #include "memory.h"
 
 
-void memory_init()
-{    
-	int Main_Array[100];
+FreeBlock* freelist;
 
-	for (int i = 0; i < 100; i++) Main_Array[i] = 0;
+void memory_init()
+{
+        int Main_Array[100];
+
+        for (int i = 0; i < 100; i++) Main_Array[i] = 0;
 
 }
 
+bool var_allocate(char name , int size)
+{
+        while(freelist != NULL)
+                {
+                        if (size >= freelist.size)
+                        {
+
+                        }
+
+
+
+                }
 
 
 
 
 
+
+
+
+}
