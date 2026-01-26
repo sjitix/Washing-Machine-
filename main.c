a8d0d7eca9adce93ac166c674868325981f66cc2
sjitix <126064940+sjitix@users.noreply.github.com>
2026-01-26 14:39:07 +0100

    Refactor main.c to include memory management

--- main.c
+++ main.c
@@ -1,3 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include "memory.h"
 #include "struct.h"
+
+int parse(const char* filename, Command* commands);
+void execute(Command cmd);
+
+int main() {
+    Command commands[1000];
+
+    memory_init();
+
+    int count = parse("test.txt", commands);
+
+    for (int i = 0; i < count; i++) {
+        execute(commands[i]);
+    }
+
+    return 0;
+}
