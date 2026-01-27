3544d71eb356ee02ce36b06e59c2ecfd20324036
sjitix <adrawing26@gmail.com>
2026-01-27 12:21:24 +0100

    Add comments back to executor.c and memory.c

--- executor.c
+++ executor.c
@@ -15,6 +15,7 @@ void execute(int index) {
 
 	switch(cmd_get_op(cmd)) {
 		case ASS:
+			//assigns n to the first space of x
 			var_write_at(v1, 0, cmd_get_number(cmd));
 			break;
 
@@ -23,10 +24,12 @@ void execute(int index) {
 					error_undefined_variable();
 			if(cmd_get_number(cmd) < 0 || cmd_get_number(cmd) >= var_size(v1))
 					error_wrong_memory();
+			//writes in the nth space denoted by x,the same number add 1
 			var_write_at(v1, cmd_get_number(cmd), var_read_at(v1, cmd_get_number(cmd)) + 1);
 			break;
 
 		case DEC:
+			//writes in the nth space denoted by x, the same number minus 1
 			var_write_at(v1, cmd_get_number(cmd), var_read_at(v1, cmd_get_number(cmd)) - 1);
 			break;
 
@@ -37,28 +40,34 @@ void execute(int index) {
 			break;
 
 		case PRI:
+			//reads the nth space denoted by x and prints it
 			printf("%d\n", var_read_at(v1, cmd_get_number(cmd)));
 			break;
 
 		case ADD:
+			//overwrites the first cell of x with the result of the addition of the first cells of x and y
 			var_write_at(v1, 0, var_read_at(v1, 0) + var_read_at(v2, 0));
 			break;
 
 		case SUB:
+			//overwrites the first cell of x with the result of the subtraction of the first cells of x and y
 			var_write_at(v1, 0, var_read_at(v1, 0) - var_read_at(v2, 0));
 			break;
 
 		case MUL:
+			//overwrites the first cell of x with the result of the multiplication of the first cells of x and y
 			var_write_at(v1, 0, var_read_at(v1, 0) * var_read_at(v2, 0));
 			break;
 
 		case AND:
+			//overwriting each element of x[i] with the value of (x[i]*y[i]%2)
 			for (i = 0; i < var_size(v1); i++) {
 				var_write_at(v1, i, (var_read_at(v1, i) * var_read_at(v2, i)) % 2);
 			}
 			break;
 
 		case XOR:
+			//overwriting each element of x[i] with the value of (x[i]+y[i]%2)
 			for (i = 0; i < var_size(v1); i++) {
 				var_write_at(v1, i, (var_read_at(v1, i) + var_read_at(v2, i)) % 2);
 			}
@@ -69,6 +78,7 @@ void execute(int index) {
 			break;
 
 		case PRA:
+			//prints contents of a variable
 			printf("[");
 			for (i = 0; i < var_size(v1); i++) {
 				printf(" %d", var_read_at(v1, i));
--- memory.c
+++ memory.c
@@ -15,6 +15,7 @@ typedef struct FreeBlock {
 	struct FreeBlock* next;
 } FreeBlock;
 
+// list of free spaces
 FreeBlock* freelist;
 
 struct internalVar var_table[256];
@@ -22,17 +23,22 @@ struct internalVar var_table[256];
 void memory_init()
 {
 	freelist = malloc ( sizeof(struct FreeBlock));
+	//initially the whole list is one free block
 	freelist -> start = 0;
 	freelist -> size = 100;
 	freelist -> next = NULL;
 
+	//start of a variable = -1 means variable doesn't exist
 	for(int i = 0 ; i < 256 ; i++)var_table[i].start = -1;
 }
 
+//this function merges adjacent blocks of free space
 void coalesce (FreeBlock *  previous , FreeBlock * new_node)
 {
         FreeBlock * temp;
 
+        //if the end of the node is equal to the start of the next
+        //merge them
         if( (new_node -> next != NULL) && (new_node -> start + new_node -> size == new_node -> next -> start))
         {
                 temp = new_node -> next;
@@ -44,6 +50,8 @@ void coalesce (FreeBlock *  previous , FreeBlock * new_node)
                 free(temp);
         }
 
+        //if the start of the node is equal to the end of the previous
+        //merge them
         if( (previous != NULL) && (previous -> start + previous -> size == new_node -> start) )
         {
                 previous -> size = previous -> size + new_node -> size;
@@ -64,36 +72,48 @@ int var_allocate(char name , int size)
 
         int start_free_node;
 
+        //iterate trough the free list
         while(current != NULL)
         {
+                //if the size of the node is >= than the size needed
+                //suitable empty space has been found
                 if (current -> size >= size)
                 {
                         start_free_node = current -> start;
 
+                        //if size of node is exact
                         if(current -> size == size)
                         {
+                                //remove node from freelist
+                                //case1: its the first free space in the list
                                 if(previous == NULL)
                                 {
                                         freelist = current -> next;
                                         free(current);
                                 }
+                                //case2: its not first node in freelist
                                 else
                                 {
                                         previous -> next = current -> next;
                                         free(current);
                                 }
                         }
+                        //size of node is bigger -> truncate the node
                         else
                         {
+                                //add to the start
                                 current -> start = current -> start + size;
+                                //substract to have remaining space in the node
                                 current -> size = current -> size - size;
                         }
 
+                        //initialize with 0 - the actual alocation
                         for (int i = start_free_node;  i < start_free_node + size; i++)
                         {
                                 Main_Array[i] = 0;
                         }
 
+                        //update variables
                         var_table[name].start = start_free_node;
                         var_table[name].size = size;
 
@@ -111,6 +131,7 @@ int var_free(char name)
 {
         FreeBlock * new_free_node;
 
+        //if the variable already doesnt exist , cannot free it
         if(var_table[name].start == -1) return 0;
 
         new_free_node = malloc (sizeof(struct FreeBlock));
@@ -121,31 +142,41 @@ int var_free(char name)
         current = freelist;
         previous = NULL;
 
+        //set new free node in the list based on the freed variable
         new_free_node -> start  = var_table[name].start;
         new_free_node -> size = var_table[name].size;
 
+        //variable doesnt exist
         var_table[name].start = -1;
 
+        // if freelist is empty already
         if (freelist == NULL)
         {
+                //insert at head
                 new_free_node -> next = NULL;
                 freelist = new_free_node;
                 coalesce(previous , new_free_node);
                 return 1;
         }
 
+        //find the right position to insert new free node
         while(current != NULL)
         {
+                //if smaller than first empty space
                 if(new_free_node -> start < current -> start && previous == NULL)
                 {
+                        //add it as the first node of freelist
                         new_free_node -> next = current ;
                         freelist = new_free_node;
+                        //merge freespaces
                         coalesce(previous , new_free_node);
                         return 1;
                 }
 
                 else if( previous != NULL)
                 {
+                        //if it new free space bewteen previous and current
+                        // add between them
                         coalesce(previous, new_free_node);
                         return 1;
                 }
@@ -154,8 +185,11 @@ int var_free(char name)
                 current = current -> next;
         }
 
+        //if no suitable space has been found
+        //put it at the end of the empty list
         previous -> next = new_free_node;
         new_free_node -> next = NULL;
+        //merge freespaces
         coalesce(previous, new_free_node);
 
         return 1;
