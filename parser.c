68e79ed7eb731d16fc3e15f3b9573c30499f893e
Indigo-Morgenstern <mzcarrots@gmail.com>
2026-01-28 14:09:25 +0100

    Update parser.c

--- parser.c
+++ parser.c
@@ -2,18 +2,18 @@
 #include <string.h>
 #include "parser.h"
 
-struct internalCommand {
+typedef struct internalCommand {
     int op;
     char var1;
     char var2;
     int number;
-}commands[100];
+} command;
 
 
 
 Command get_command(int index)
 {
-    return &commands[index];
+    return &command;
 }
 
 int cmd_get_op(Command cmd)
@@ -42,71 +42,57 @@ int parse (const char * filename)
 
     if(file == NULL) return 0;
 
-    int count = 0;
+    fscanf(file, " %c %d", &command.var1, &commands[count].number);
 
-    char opcode[4];
-
-    while(fscanf(file , "%s", opcode) == 1)
+    while(fscanf(file , "%s") == 1)
     {
         if(strcmp(opcode, "Mal") == 0)
         {
-            commands[count].op = MAL;
-            fscanf(file, " %c %d", &commands[count].var1, &commands[count].number);
+            command.op = MAL;
         }
         else if(strcmp(opcode, "Ass") == 0)
         {
-            commands[count].op = ASS;
-            fscanf(file, " %c %d", &commands[count].var1, &commands[count].number);
+            command.op = ASS;
         }
         else if(strcmp(opcode, "Inc") == 0)
         {
             commands[count].op = INC;
-            fscanf(file, " %c %d", &commands[count].var1, &commands[count].number);
         }
         else if(strcmp(opcode, "Dec") == 0)
         {
             commands[count].op = DEC;
-            fscanf(file, " %c %d", &commands[count].var1, &commands[count].number);
         }
         else if(strcmp(opcode, "Pri") == 0)
         {
             commands[count].op = PRI;
-            fscanf(file, " %c %d", &commands[count].var1, &commands[count].number);
         }
         else if(strcmp(opcode, "Add") == 0)
         {
             commands[count].op = ADD;
-            fscanf(file, " %c %c", &commands[count].var1, &commands[count].var2);
         }
         else if(strcmp(opcode, "Sub") == 0)
         {
             commands[count].op = SUB;
-            fscanf(file, " %c %c", &commands[count].var1, &commands[count].var2);
         }
         else if(strcmp(opcode, "Mul") == 0)
         {
             commands[count].op = MUL;
-            fscanf(file, " %c %c", &commands[count].var1, &commands[count].var2);
         }
         else if(strcmp(opcode, "And") == 0)
         {
             commands[count].op = AND;
-            fscanf(file, " %c %c", &commands[count].var1, &commands[count].var2);
         }
         else if(strcmp(opcode, "Xor") == 0)
         {
             commands[count].op = XOR;
-            fscanf(file, " %c %c", &commands[count].var1, &commands[count].var2);
         }
         else if(strcmp(opcode, "Fre") == 0)
         {
             commands[count].op = FRE;
-            fscanf(file, " %c", &commands[count].var1);
         }
         else if(strcmp(opcode, "Pra") == 0)
         {
             commands[count].op = PRA;
-            fscanf(file, " %c", &commands[count].var1);
         }
 
         count ++;
