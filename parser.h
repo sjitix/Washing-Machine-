eb67b4a4221c941f0857d80e42bf2837f0c6e851
Indigo-Morgenstern <mzcarrots@gmail.com>
2026-01-29 23:28:57 +0100

    Update parser.h
    more comments

--- parser.h
+++ parser.h
@@ -15,19 +15,26 @@
 #define FRE 10
 #define PRA 11
 
-// 
+// represents a line of code in the mini-language
 typedef struct internalCommand * Command;
 
+// inputs the code file; outputs an integer representing the number of lines of code
+// parses code into an array of commands in the sequence they are called in the mini-language
 int parse(const char * filename);
 
+// inputs the index of a given command in the array of commands; outputs that given command
 Command get_command(int index);
 
+// inputs command instance; outputs the operation (e.g. MAL, DEC, etc) carried out by that command
 int cmd_get_op(Command cmd);
 
+// inputs command instance; outputs the first parameter of the command
 char cmd_get_var1(Command cmd);
 
+// inputs command instance; outputs the second parameter of the command (if it is a character)
 char cmd_get_var2(Command cmd);
 
+// inputs command instance; outputs the second parameter of the command (if it is an integer)
 int cmd_get_number(Command cmd);
 
 #endif
